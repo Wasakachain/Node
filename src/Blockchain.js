d381571460db5ade4c1ca3f624f728591781daa9
Angel <angelafonso@oxoft.io>
2019-11-06 21:11:30 -0400

    updated blockchain model and transaction model

--- src/Blockchain.js
+++ src/Blockchain.js
@@ -1,57 +1,36 @@
+const responseData = require('../utils/functions').responseData;
 const Block = require('./Block');
-const Transaction = require('./transaction');
-const { sha256 } = require('../utils');
-const { request } = require('../utils');
+const initialDifficulty = require('../global').initialDifficulty;
 
 class Blockchain {
     constructor() {
         this.chain = [];
-        this.pendingTransaction = [];
-        this.peers = [];
+        this.pendingTransactions = [];
+        this.confirmedTransactions = [];
+        this.blocksCount = 0;
+        this.nodes = [];
+
+        //Create genesis block
+        this.chain.push(new Block({
+            index: 0,
+            prevBlockHash: '0',
+            previousDifficulty: 0,
+            pendingTransactions: this.pendingTransactions,
+            nonce: 0,
+            minedBy: '00000000000000000000000000000000'
+        }));
+        this.getBlock = this.getBlock.bind(this);
     }
 
-    get lastBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    newTransaction(from, to, value, fee, dateCreated, data, senderPubKey) {
-        this.pendingTransaction.push(new Transaction(from, to, value, fee, dateCreated, data, senderPubKey));
-        // return this.lastBlock.index + 1;
-    }
-
-    registerNode(url) {
-        this.peers.push(url);
-    }
-
-    addBlock(req, response) {
-        return response.send({ message: `block added` });
-    }
-
-    validChain(chain) {
-        let lastBlock = this.blockchain[0];
-        for (let currentIndex = 1; currentIndex < this.blockchain.length; currentIndex++) {
-            let block = this.blockchain[currentIndex];
-
-            if (block.previousHash !== sha256(JSON.stringify(lastBlock))) {
-                console.log(`Previous hash does not match on block ${currentIndex}`);
-                return false;
-            }
-
-            if (Blockchain.validProof(block)) {
-                console.log(`Invalid proof of work on block ${currentIndex}`);
-                return false;
-            }
+    getBlock(req, response) {
+        if (!req.params.index || !this.chain[req.params.index]) {
+            return response
+                .status(404)
+                .json(responseData({ message: 'Block not found' }));
         }
+        return response.json(responseData(this.chain[req.params.index]));
     }
 
-    resolveConflict() {
-        let newChain = null;
-        let maxLenght = this.chain;
-
-        this.peers.forEach((node) => {
-            let res = request()
-        });
-    }
 }
 
 module.exports = Blockchain;
--- src/Transaction.js
+++ src/Transaction.js
@@ -1,70 +1,29 @@
 const { sha256 } = require('../utils');
-const TRANSACTION_STATUS = {
-    'pending': 'pending',
-    'confirmed': 'confirmed',
-};
 
-function Transaction(_from, _to, _value, _fee, _senderPubKey, _data, _senderSignature) {
-    var from = _from;
-    var to = _to;
-    var value = _value;
-    var fee = _fee;
-    var dateCreated = new Date().toISOString();
-    var data = _data;
-    var senderPubKey = _senderPubKey;
-    var transationDataHash = null;
-    var senderSignature = _senderSignature;
-
+function Transaction(from, to, value, fee, senderPubKey, data, senderSignature) {
     class SingleTransaction {
         constructor() {
-            transationDataHash = sha256(JSON.stringify({
+            this.dateCreated = new Date().toISOString();
+
+            this.transationDataHash = sha256(JSON.stringify({
                 from,
                 to,
                 value,
                 fee,
-                dateCreated,
+                dateCreated: this.dateCreated,
                 data,
                 senderPubKey,
             }));
         }
-        index(_, response) {
-            return response.send({ message: 'this are all the transactions' });
-        }
 
-        show(req, response) {
-            return response.send({ message: `this is the transaction hash number: ${req.params.hash}` });
-        }
-
-        pendingTransactions(_, response) {
-            return response.send({ message: 'this are all the transactions in pending state' });
-        }
-
-        confirmedTransactions(_, response) {
-            return response.send({ message: 'this are all the transactions in confirmed state' });
-        }
-
-        send(_, response) {
-            return response.send({ message: 'transaction done!' });
-        }
-
-        addressBalance(req, response) {
-            return response.send({ message: `this is the address ${req.params.address} balance` });
-        }
-
-        addressTransactions(req, response) {
-            return response.send({ message: `this is the address ${req.params.address} transactions` });
-        }
-
-        get data() {
+        getData() {
             return {
                 from,
                 to,
                 value,
                 fee,
-                dateCreated,
                 data,
                 senderPubKey,
-                transationDataHash,
                 senderSignature,
             }
         }
