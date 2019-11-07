1abcd2d12ff44a231a9340c3199135c62c93167c
Francisco Sarmiento <frank.jav.sm@gmail.com>
2019-11-06 21:47:40 -0400

    block structure update

--- src/Block.js
+++ src/Block.js
@@ -1,32 +1,32 @@
-class Block {
-    constructor(index, transactions, difficulty, prevBlockHash, minedBy, blockDataHash, nonce, dateCreated, blockHash) {
-        this.index = index;
-        this.transactions = transactions;
-        this.difficulty = difficulty;
-        this.prevBlockHash = prevBlockHash;
-        this.minedBy = minedBy;
-        this.blockDataHash = blockDataHash;
-        this.nonce = nonce;
-        this.dateCreated = dateCreated;
-        this.blockHash = blockHash;
-    }
+const { sha256 } = require('../utils')
 
-    index(req, response) {
-        this.proof = proof;
-        this.previousHash = previousHash;
-        this.nodes = [];
-        // binding methods
-        this.__index = this.__index.bind(this);
-        this.__show = this.__show.bind(this);
-    }
+function Block({ index, difficulty, prevBlockHash, transactions, nonce, minedBy }) {
+    let blockDataHash = null;
+    let dateCreated = null;
+    let blockHash = null;
+    class BlockClass {
+        constructor() {
+            blockDataHash = sha256(JSON.stringify({ index, transactions, difficulty, prevBlockHash, minedBy }));
+            dateCreated = new Date().toISOString();
+            blockHash = sha256(JSON.stringify({ blockDataHash, dateCreated, nonce }));
+        }
 
-    index(req, response) {
-        return response.send({ message: 'hello world' });
+        getData() {
+            return {
+                index,
+                transactions,
+                difficulty,
+                prevBlockHash: prevBlockHash,
+                minedBy,
+                blockDataHash,
+                nonce,
+                dateCreated,
+                blockHash,
+            }
+        }
     }
-
-    show(req, response) {
-        return response.send({ message: `this is the block number: ${req.params.index}` });
-    }
+    return new BlockClass();
 }
+
 
 module.exports = Block;
