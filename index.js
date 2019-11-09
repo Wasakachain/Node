faaa3d8eec7ea25d0fdcf7ee999374269c66ccd7
Angel <angelafonso@oxoft.io>
2019-11-08 22:52:14 -0400

    WIP added functions to handle peers connections

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const Express = require('express');
 const App = Express();
 // importing routes
 const Routes = require('./src/Routes/NodeRoutes');
+
+App.use(Express.json());
+App.use(Express.urlencoded({ extended: true }));
+
 // App routes
 App.use('/', Routes);
 
+// App.use(Express.json);
 // turn on the server
-const nodeServer = App.listen(5555, () => {
-    console.log('\x1b[36m%s\x1b[0m','Node server is running!');
-    console.log('\x1b[42m%s\x1b[0m','\t\t\t-/-/-/-¡Enjoy WasakaChain!-/-/-/\t\t\t');
+App.listen(process.env.port, () => {
+    console.log('\x1b[36m%s\x1b[0m', 'Node server is running on port ' + process.env.port + '!');
+    console.log('\x1b[42m%s\x1b[0m', '\t\t\t-/-/-/-¡Enjoy WasakaChain!-/-/-/\t\t\t');
 });
