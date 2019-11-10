923e98dd17f58f231465559e172775aa71a8b14b
Angel <angelafonso@oxoft.io>
2019-11-10 14:31:33 -0400

    added documentation blocks to transaction class

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,7 +1,21 @@
 const { sha256, verifySignature } = require('../utils/hash');
 const { isValidAddress } = require('../utils/functions');
+
 class Transaction {
-    constructor(from, to, value, fee, senderPubKey, data, senderSignature, minedInBlockIndex, transferSuccessful = false) {
+    /**
+     * Transaction representation class
+     * @param {string} from transaction sender address
+     * @param {stirng} to transaction receiver address
+     * @param {number} value transaction amount
+     * @param {number} fee transaction fee
+     * @param {string} senderPubKey sender public key
+     * @param {Array} senderSignature sender signature
+     * @param {number} minedInBlockIndex index of the block where the transaction will be placed
+     * @param {boolean} transferSuccessful  
+     * @param {boolean} isCoinbase  set to true when transaction is a coinbase transaction
+     * @param {string} data transaction data
+     */
+    constructor(from, to, value, fee, senderPubKey, senderSignature, minedInBlockIndex, transferSuccessful = false, isCoinbase = false, data) {
         this.from = from;
         this.to = to;
         this.value = value;
@@ -9,33 +23,33 @@ class Transaction {
         this.dateCreated = new Date().toISOString();
         this.data = data;
         this.senderPubKey = senderPubKey;
-        this.transactionDataHash = sha256(JSON.stringify({
-            from,
-            to,
-            value,
-            fee,
-            dateCreated: this.dateCreated,
-            data,
-            senderPubKey,
-        }));
-
+        this.transactionDataHash = Transaction.dataHash({ from, to, value, fee, dateCreated, senderPubKey });
+        this.isCoinbase = isCoinbase;
         this.senderSignature = senderSignature;
         this.minedInBlockIndex = minedInBlockIndex;
         this.transferSuccessful = transferSuccessful;
 
     }
 
+    /**
+     * returns true when the transaction has a valid signature
+     * @returns {boolean}
+     */
     verify() {
         return verifySignature(this.transactionDataHash, this.senderPubKey, this.senderSignature);
     }
 
+    /**
+     * returns true when the given transaction is a valid transaction
+     * @param {Transaction} transaction transaction to check
+     */
     static isValid(transaction) {
         //TO DO: COMPLETE THIS METHOD
         if (!isValidAddress(transaction.from) && !isValidAddress(transaction.to)) {
             return false;
         }
 
-        if (Transaction.isValid(transaction) !== transaction.transactionDataHash) {
+        if (Transaction.dataHash(transaction) !== transaction.transactionDataHash) {
             return false;
         }
 
@@ -43,22 +57,10 @@ class Transaction {
 
     }
 
-    getData() {
-        return {
-            from,
-            to,
-            value,
-            fee,
-            dateCreated,
-            data,
-            senderPubKey,
-            transactionDataHash,
-            senderSignature,
-            minedInBlockIndex,
-            transferSuccessful
-        }
-    }
-
+    /**
+     * Calculate the data hash of the given transaction and return it
+     * @param {Transaction}  transaction
+     */
     static dataHash({ from, to, value, fee, dateCreated, data, senderPubKey }) {
         return sha256(JSON.stringify({
             from,
@@ -71,7 +73,15 @@ class Transaction {
         }))
     }
 
-    static coinbaseTransaction({ to, value, fee, data, minedInBlockIndex }) {
+    /**
+     * Create a coinbase transaction
+     * @param {string} to address of transaction receiver
+     * @param {number} value transaction amount
+     * @param {number} fee transaction fee
+     * @param {number} minedInBlockIndex index of the block where the transaction will be placed
+     * @param {string} data transaction data
+     */
+    static coinbaseTransaction(to, value, fee, minedInBlockIndex, data) {
         const from = '0000000000000000000000000000000000000000',
             senderPubKey = '00000000000000000000000000000000000000000000000000000000000000000',
             senderSignature = [
@@ -98,81 +108,3 @@ class Transaction {
 }
 
 module.exports = Transaction;
-/*
-const { sha256 } = require('../utils/hashes');
-
-const TRANSACTION_STATUS = {
-    'pending': 'pending',
-    'confirmed': 'confirmed',
-};
-
-class Transaction {
-    constructor({ from, to, value, fee, senderPubKey, data, senderSignature, minedInBlockIndex }) {
-        this.from = from;
-        this.to = to;
-        this.value = value;
-        this.fee = fee;
-        this.senderPubKey = senderPubKey;
-        this.data = data.trim();
-        this.senderSignature = senderSignature;
-        this.dateCreated = new Date().toISOString();
-        this.minedInBlockIndex = minedInBlockIndex;
-
-    }
-
-    getData() {
-        const { from, to, value, fee, data, dateCreated, senderPubKey, senderSignature } = this;
-
-
-        return {
-            from,
-            to,
-            value,
-            fee,
-            dateCreated: dateCreated,
-            ...Object.assign({}, data ? { data } : {}),
-            senderPubKey,
-            transactionDataHash: Transaction.getTransactionDataHash(this),
-            senderSignature
-        }
-    }
-
-    static getTransactionDataHash({ from, to, value, fee, data, dateCreated, senderPubKey }) {
-        return sha256(JSON.stringify({
-            from,
-            to,
-            value,
-            fee,
-            dateCreated,
-            ...Object.assign({}, data ? { data } : {}),
-            senderPubKey,
-        }))
-    }
-
-    static getCoinbaseTransaction({ to, value, fee, data, minedInBlockIndex }) {
-        const from = '0000000000000000000000000000000000000000',
-            senderPubKey = '00000000000000000000000000000000000000000000000000000000000000000',
-            senderSignature = [
-                '0000000000000000000000000000000000000000000000000000000000000000',
-                '0000000000000000000000000000000000000000000000000000000000000000'
-            ],
-            dateCreated = new Date().toISOString();
-
-        return {
-            from,
-            to,
-            value,
-            fee,
-            dateCreated: dateCreated,
-            ...Object.assign({}, data ? { data: data.trim() } : {}),
-            senderPubKey,
-            transactionDataHash: Transaction.getTransactionDataHash({
-                to, value, fee, data: data.trim(), minedInBlockIndex, from, senderPubKey, senderSignature, dateCreated
-            }),
-            senderSignature,
-            minedInBlockIndex
-        }
-    }
-}
-
-module.exports = Transaction; */
